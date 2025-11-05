80f65a6222896278b6789341646890817279b649
unknown <199391516+Brandondiaz18@users.noreply.github.com>
2025-11-05 17:17:35 -0500

    Configuración GitHub Pages actualizada

--- set-public-url.js
+++ set-public-url.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const isVercel = process.env.VERCEL === "1";
 const homepage = isVercel
   ? "" // En Vercel, el sitio vive en el dominio raíz
-  : "https://JuanRojasB.github.io/Proyecto"; // En GitHub Pages, usamos la subcarpeta
+  : "https://brandondiaz18.github.io/Proyecto_Jueves/"; // En GitHub Pages, usamos la subcarpeta
 
 const envContent = `PUBLIC_URL=${homepage}\n`;
 fs.writeFileSync("./.env.production", envContent);
