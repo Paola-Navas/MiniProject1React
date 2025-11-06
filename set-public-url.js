dd671ce622bf7bf640179dfdbb0d3965eb6dfa6f
Paola Navas <Paola-Navas@users.noreply.github.com>
2025-11-06 10:49:21 -0500

    arreglos

--- set-public-url.js
+++ set-public-url.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const isVercel = process.env.VERCEL === "1";
 const homepage = isVercel
   ? "" // En Vercel, el sitio vive en el dominio raíz
-  : "https://brandondiaz18.github.io/Proyecto_Jueves/"; // En GitHub Pages, usamos la subcarpeta
+  : "https://paola-navas.github.io/MiniProject1React/"; // En GitHub Pages, usamos la subcarpeta
 
 const envContent = `PUBLIC_URL=${homepage}\n`;
 fs.writeFileSync("./.env.production", envContent);
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto/', // 👈 importante: debe tener slash inicial y final
+  base: '/Proyecto/', 
 })
 
